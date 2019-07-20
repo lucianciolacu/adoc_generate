bd4b5dc73f26c149f75fc6794c7eac9a1a5ec26a
Lucian Ciolacu <lciolacu1401@gmail.com>
2019-07-20 12:55:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+adoc_generate
+=============
+
+Scaffolding tool to generate AsciiDoctor files in the Antora structure.
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/adoc_generate.svg)](https://npmjs.org/package/adoc_generate)
+[![Downloads/week](https://img.shields.io/npm/dw/adoc_generate.svg)](https://npmjs.org/package/adoc_generate)
+[![License](https://img.shields.io/npm/l/adoc_generate.svg)](https://github.com/lucianciolacu/adoc_generate/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g adoc_generate
+$ adoc_generate COMMAND
+running command...
+$ adoc_generate (-v|--version|version)
+adoc_generate/0.0.1 linux-x64 node-v10.16.0
+$ adoc_generate --help [COMMAND]
+USAGE
+  $ adoc_generate COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+* [`adoc_generate generate`](#adoc_generate-generate)
+* [`adoc_generate help [COMMAND]`](#adoc_generate-help-command)
+
+## `adoc_generate generate`
+
+Generates the Antora Structure
+
+```
+USAGE
+  $ adoc_generate generate
+
+OPTIONS
+  -n, --reponame=reponame  Name of the repository
+```
+
+_See code: [src/commands/generate.js](https://github.com/lucianciolacu/adoc_generate/blob/v0.0.1/src/commands/generate.js)_
+
+## `adoc_generate help [COMMAND]`
+
+display help for adoc_generate
+
+```
+USAGE
+  $ adoc_generate help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.2.0/src/commands/help.ts)_
+<!-- commandsstop -->
---
+++ src/commands/generate.js
@@ -0,0 +1,51 @@
+const {Command, flags} = require('@oclif/command');
+const fs = require('fs');
+const path = require('path');
+
+
+
+var currentPath = process.cwd();
+
+
+class generateCommand extends Command {
+  async run() {
+    const {flags} = this.parse(generateCommand);
+    const repoName = flags.reponame
+    //create repo root dir
+    console.log('Creating project root in:' + currentPath + '/' + repoName);
+    fs.mkdirSync(currentPath + '/' + repoName, { recursive: true });
+    //create modules folders & rest of the structure
+    //pages + partials
+    fs.mkdirSync(currentPath + '/' + repoName + '/modules' + '/ROOT' + '/pages' + '/_partials', { recursive: true });
+    //assets + images
+    fs.mkdirSync(currentPath + '/' + repoName + '/modules' + '/ROOT' + '/assets' + '/images', { recursive: true });
+    //this.log(`generate ${name} from ./src/commands/generate.js`)
+    //Copy template files
+    //YML files
+    //tooling/templates/modules/ROOT/assets/images/asciidoc.jpg
+    var templates = "templates"
+    const sourcePaths = [templates, templates, templates + '/modules/ROOT', templates + '/modules/ROOT/assets/images', templates + '/modules/ROOT/pages', templates + '/modules/ROOT/pages/_partials'];
+    const files = ['/antora.yml', '/playbook.yml', '/nav.adoc', '/asciidoc.jpg', '/index.adoc', '/partial.adoc'];
+    const destination = currentPath + '/' + repoName
+    const destPaths = [destination, destination, destination + '/modules/ROOT', destination + '/modules/ROOT/assets/images', destination + '/modules/ROOT/pages', destination + '/modules/ROOT/pages/_partials'];
+    sourcePaths.forEach(function (item, index) {
+      console.log("Copying source file: " + item + files[index]);
+      console.log("to: " + destPaths[index] + files[index]);
+      fs.copyFileSync(item + files[index], destPaths[index] + files[index]);
+    });
+
+
+
+    //fs.copyFileSync(sourceFiles, sourceFiles[1]);
+
+  }
+}
+
+generateCommand.description = `Generates the Antora Structure`
+
+generateCommand.flags = {
+  reponame: flags.string({char: 'n', description: 'Name of the repository'}),
+  //name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = generateCommand
